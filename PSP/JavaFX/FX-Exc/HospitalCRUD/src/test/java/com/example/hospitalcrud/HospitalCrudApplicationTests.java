e6e2fcad7f345eeb41813a42a340a84e72c9b894
Adrian Saavedra <153601542+cronoxxxx@users.noreply.github.com>
2024-09-24 13:18:50 +0200

    adas

---
+++ PSP/JavaFX/FX-Exc/HospitalCRUD/src/test/java/com/example/hospitalcrud/HospitalCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospitalcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
